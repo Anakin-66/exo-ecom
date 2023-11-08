3c4ebab7cea355ea786295e732ad29926d6c893e
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-08 10:39:08 +0100

    added products cart

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,17 @@
 function Header () {
     const userOnline = true
-    const name = 'Nathan'
+    const name = 'Nathan';
+    const ProductsInCart = 5;
     return (
         <header>
             <nav>
                 <lu>
+                    { userOnline ? <li>{name}</li> : <li>Veuillez vous connecter</li> }
+
                     <li>Home</li>
                     <li>Products</li>
-                    { userOnline ? <li>{name}</li> : <li>Veuillez vous connecter</li> }
                     
+                    { ProductsInCart > 0 ? <li>Cart : {ProductsInCart} </li> : <li>Pas de produits dans le panier</li> }
                 </lu>
             </nav>
         </header>
